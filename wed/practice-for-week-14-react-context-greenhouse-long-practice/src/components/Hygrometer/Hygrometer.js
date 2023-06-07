0f770dd82c2a72d26aee5ef9db3782566b72015d
gisales92 <isales.greg@gmail.com>
2023-06-07 15:55:06 -0700

    gross nested ternary for bonus 2

--- wed/practice-for-week-14-react-context-greenhouse-long-practice/src/components/Hygrometer/Hygrometer.js
+++ wed/practice-for-week-14-react-context-greenhouse-long-practice/src/components/Hygrometer/Hygrometer.js
@@ -1,17 +1,38 @@
 import ReactSlider from "react-slider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ClimateContext } from "../../context/ClimateContext";
 import "./Hygrometer.css";
 
 function Hygrometer() {
-  const {humidity, setHumidity} = useContext(ClimateContext);
+  const { humidity, setHumidity } = useContext(ClimateContext);
+  const [desiredHum, setDesiredHum] = useState(humidity);
+
+  useEffect(() => {
+    if (humidity !== desiredHum) {
+      const humTimeout = setTimeout(() => {
+        // Enjoy this nested ternary -- Since we're decrementing humidity by 2, I'm setting up a series of checks to see if we are only one away from the desired humidity so there is not a situation were we keep going one above and below the desired humidity
+        desiredHum > humidity
+          ? desiredHum > humidity + 1
+            ? setHumidity(humidity + 2)
+            : setHumidity(humidity + 1)
+          : desiredHum < humidity - 1
+          ? setHumidity(humidity - 2)
+          : setHumidity(humidity - 1);
+
+        return () => clearTimeout(humTimeout);
+      }, 1000);
+    }
+  }, [desiredHum, humidity]);
+
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={humidity}
-        onAfterChange={(val) => {setHumidity(val)}}
+        value={desiredHum}
+        onAfterChange={(val) => {
+          setDesiredHum(val);
+        }}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
